4e1b892f147fbe402a5a851c525dda5041edd4ac
bkd <130774364+lawdachuss@users.noreply.github.com>
2026-09-07 11:52:22 +0530

    chore: remove legacy scripts and lib/db module

--- nofity_pgrst.mjs
+++
@@ -1,8 +0,0 @@
-import pg from 'pg';
-const pool = new pg.Pool({
-  connectionString: '***REMOVED***',
-  ssl: { rejectUnauthorized: false }
-});
-await pool.query("NOTIFY pgrst, 'reload schema'");
-console.log('Schema cache reload notified');
-await pool.end();
